d92b5bd772c0378583c50be752e4b133e1fd6f67
Yuta Hongo <yutago@gmail.com>
2016-12-05 04:30:01 +0900

    Scrape Github

---
+++ scrape_github/spiders/github.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from urllib.parse import urljoin
+
+class GithubSpider(scrapy.Spider):
+    name = "github"
+    allowed_domains = ["api.github.com"]
+    base_url = 'https://api.github.com/'
+
+    def start_requests(self):
+        user = self.user
+        # TODO Return error message if 'user' command line parameter is not passed
+        urls = ["users/%s/events/public" % user, "/users/%s/starred" % user, "/users/%s/repos" % user, "/users/%s/followers" % user, "/users/%s/following" % user]
+        for path in urls:
+            url = urljoin(self.base_url, path)
+            yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        key = response.url.split("/")[-1]
+        yield json.loads("{ \"%s\": %s }" % (key, response.body.decode('utf-8')))
